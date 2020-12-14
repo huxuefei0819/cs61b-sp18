c6a1eda9d7ae3db3834499010bf4fafa45f0fd8f
Xuefei Hu <hux3@upmc.edu>
2020-12-14 13:33:38 -0500

    .

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,6 +1,7 @@
 package lab11.graphs;
 
 import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
 
 /**
@@ -15,6 +16,7 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     private int s;
     private int t;
     private Maze maze;
+    private Queue<Integer> fringe;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
@@ -23,6 +25,7 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
         t = maze.xyTo1D(targetX, targetY);
         distTo[s] = 0;
         edgeTo[s] = s;
+        fringe = new LinkedList<>();
     }
 
     /**
@@ -33,13 +36,12 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
         if (s == t) {
             return;
         }
-        Queue<Integer> fringe = new ArrayDeque<>();
+
         fringe.add(s);
         while (!fringe.isEmpty()) {
             int v = fringe.poll();
             for (int w : maze.adj(v)) {
                 if (!marked[w]) {
-                    fringe.add(w);
                     marked[w] = true;
                     edgeTo[w] = v;
                     distTo[w] = distTo[v] + 1;
@@ -47,6 +49,8 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
                 }
                 if (w == t) {
                     return;
+                } else {
+                    fringe.add(w);
                 }
             }
         }
